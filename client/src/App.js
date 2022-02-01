ddaa77c5fab6a0a1f20aa455d88f7874b50457db
pvm12 <pvm121212@gmail.com>
2022-02-01 14:44:15 +0200

    скрипт для билда приложения и запуска сервера

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,45 @@
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-        <h1> The Wall </h1>
+import Form from "./components/Form";
+import List from "./components/List";
+
+class App extends React.Component {
+  state = {
+      bricks: []
+  }
+  componentDidMount() {
+      this.fetchAllBricks();
+  }
+
+  fetchAllBricks = () => {
+      fetch('/api/bricks').then(response =>response.json()).then(bricks =>{
+          this.setState({ bricks });
+      });
+  }
+
+  addNewBrick = (text) => {
+      fetch('/api/bricks', {
+          method: 'POST',
+          body: JSON.stringify({text}),
+          headers: {
+              'Content-type': 'application/json'
+          }
+          })
+          .then(response => response.json())
+          .then(() => this.fetchAllBricks());
+  }
+
+  render(){
+    const { bricks } = this.state;
+
+    return (
+        <div className="app">
+        <h1> The Wall. Pink Floyd </h1>
+        <Form addNewBrick={this.addNewBrick} />
+        <List bricks={bricks} />
     </div>
   );
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,8 +1,40 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', function(req, res) {
-    res.send('Server NodeJS !');
+app.use(express.json());
+let id = 3;
+
+// app.get('/', function(req, res) {
+//     res.send('Server NodeJS !');
+// })
+
+const bricks = [
+    { id: 1, text: 'Mes1'},
+    { id: 2, text: 'Mes2'},
+    { id: 3, text: 'Mes3'}
+];
+
+app.get('/api/bricks', function(req, res) {
+    // console.log('Get all bricks');
+    res.json(bricks);
+})
+
+app.post('/api/bricks', function(req, res) {
+    const body = req.body;
+    id++;
+    const newBrick = {
+        id,
+        text: body.text
+    }
+    bricks.push(newBrick)
+
+    res.json(newBrick);
+})
+//
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 app.listen(5000, () => {
